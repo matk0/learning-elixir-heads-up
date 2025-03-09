82a369f4f4a28d014a6bd14d086de0c4cb4a6bec
Matej <matej@occupationbliss.com>
2025-03-09 10:05:23 +0100

    Finish 21 Ecto Repo and Context Modules.

--- lib/heads_up/incidents.ex
+++ lib/heads_up/incidents.ex
@@ -1,43 +1,13 @@
-defmodule HeadsUp.Incident do
-  defstruct [:id, :name, :description, :priority, :status, :image_path]
-end
+alias HeadsUp.Incidents.Incident
+alias HeadsUp.Repo
 
 defmodule HeadsUp.Incidents do
   def list_incidents do
-    [
-      %HeadsUp.Incident{
-        id: 1,
-        name: "Lost Dog",
-        description: "A friendly dog is wandering around the neighborhood. 🐶",
-        priority: 2,
-        status: :pending,
-        image_path: "/images/lost-dog.jpg"
-      },
-      %HeadsUp.Incident{
-        id: 2,
-        name: "Flat Tire",
-        description: "Our beloved ice cream truck has a flat tire! 🛞",
-        priority: 1,
-        status: :resolved,
-        image_path: "/images/flat-tire.jpg"
-      },
-      %HeadsUp.Incident{
-        id: 3,
-        name: "Bear In The Trash",
-        description: "A curious bear is digging through the trash! 🐻",
-        priority: 1,
-        status: :canceled,
-        image_path: "/images/bear-in-trash.jpg"
-      }
-    ]
+    Repo.all(Incident)
   end
 
-  def get_incident(id) when is_integer(id) do
-    Enum.find(list_incidents(), fn incident -> incident.id == id end)
-  end
-
-  def get_incident(id) when is_binary(id) do
-    id |> String.to_integer() |> get_incident()
+  def get_incident!(id) do
+    Repo.get!(Incident, id)
   end
 
   def urgent_incidents(incident) do
