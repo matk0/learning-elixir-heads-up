2bbea9c297b346871c49c2b13c60d9da27ed2ad9
Matej <matej@occupationbliss.com>
2025-03-21 18:30:22 +0100

    Finish 51 Admin Users.

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,9 @@
 alias HeadsUp.Repo
 alias HeadsUp.Incidents
 alias HeadsUp.Incidents.Incident
+alias HeadsUp.Admin
+alias HeadsUp.Categories.Category
+alias HeadsUp.Accounts.User
 
 import Ecto.Query
+import Ecto.Changeset
--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -30,10 +30,13 @@ defmodule HeadsUpWeb.Router do
   end
 
   scope "/", HeadsUpWeb do
-    pipe_through [:browser, :require_authenticated_user]
+    pipe_through [:browser, :require_authenticated_user, :require_admin]
 
     live_session :admin,
-      on_mount: {HeadsUpWeb.UserAuth, :ensure_authenticated} do
+      on_mount: [
+        {HeadsUpWeb.UserAuth, :ensure_authenticated},
+        {HeadsUpWeb.UserAuth, :ensure_admin}
+      ] do
       live "/categories", CategoryLive.Index, :index
       live "/categories/new", CategoryLive.Form, :new
       live "/categories/:id", CategoryLive.Show, :show
