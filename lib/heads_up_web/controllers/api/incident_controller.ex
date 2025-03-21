a2be97210c63971cffc7f966f827ab72bf435bb9
Matej <matej@occupationbliss.com>
2025-03-21 09:47:50 +0100

    Finish 46 Generate the Auth System.

--- lib/heads_up_web/controllers/api/incident_controller.ex
+++ lib/heads_up_web/controllers/api/incident_controller.ex
@@ -20,4 +20,25 @@ defmodule HeadsUpWeb.Api.IncidentController do
       |> put_view(json: HeadsUpWeb.ErrorJSON)
       |> render(:"404")
   end
+
+  def error(%{changeset: changeset}) do
+    errors = Ecto.Changeset.traverse_errors(changeset, fn {msg, _opts} -> msg end)
+
+    %{errors: errors}
+  end
+
+  def create(conn, %{"incident" => incident_params}) do
+    case Admin.create_incident(incident_params) do
+      {:ok, incident} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", ~p"/api/incidents/#{incident}")
+        |> render(:show, incident: incident)
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:error, changeset: changeset)
+    end
+  end
 end
