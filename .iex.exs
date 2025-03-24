d2b417bd5f339443cf37820b02b5e9b1755e1be3
Matej <matej@occupationbliss.com>
2025-03-24 07:13:15 +0100

    Finish 54 Streaming New Tickets.

--- .iex.exs
+++ .iex.exs
@@ -4,6 +4,8 @@ alias HeadsUp.Incidents.Incident
 alias HeadsUp.Admin
 alias HeadsUp.Categories.Category
 alias HeadsUp.Accounts.User
+alias HeadsUp.Responses
+alias HeadsUp.Responses.Response
 
 import Ecto.Query
 import Ecto.Changeset
--- lib/heads_up_web/live/incident_live/show.ex
+++ lib/heads_up_web/live/incident_live/show.ex
@@ -2,18 +2,31 @@ defmodule HeadsUpWeb.IncidentLive.Show do
   use HeadsUpWeb, :live_view
 
   alias HeadsUp.Incidents
+  alias HeadsUp.Responses
+  alias HeadsUp.Responses.Response
+
   import HeadsUpWeb.CustomComponents
 
+  on_mount {HeadsUpWeb.UserAuth, :mount_current_user}
+
   def mount(_params, _session, socket) do
+    changeset = Responses.change_response(%Response{})
+
+    socket = assign(socket, :form, to_form(changeset))
+
     {:ok, socket}
   end
 
   def handle_params(%{"id" => id}, _uri, socket) do
     incident = Incidents.get_incident!(id)
 
+    responses = Incidents.list_responses(incident)
+
     socket =
       socket
       |> assign(:incident, incident)
+      |> stream(:responses, responses)
+      |> assign(:response_count, Enum.count(responses))
       |> assign(:page_title, incident.name)
       |> assign_async(:urgent_incidents, fn ->
         {:ok, %{urgent_incidents: Incidents.urgent_incidents(incident)}}
@@ -39,13 +52,44 @@ defmodule HeadsUpWeb.IncidentLive.Show do
               {@incident.priority}
             </div>
           </header>
+          <div class="totals">
+            {@response_count} Responses
+          </div>
           <div class="description">
             {@incident.description}
           </div>
         </section>
       </div>
       <div class="activity">
-        <div class="left"></div>
+        <div class="left">
+          <div :if={@incident.status == :pending}>
+            <%= if @current_user do %>
+              <.form for={@form} id="response-form" phx-change="validate" phx-submit="save">
+                <.input
+                  field={@form[:status]}
+                  type="select"
+                  prompt="Choose a status"
+                  options={[:enroute, :arrived, :departed]}
+                />
+
+                <.input field={@form[:note]} type="textarea" placeholder="Note..." autofocus />
+
+                <.button>Post</.button>
+              </.form>
+            <% else %>
+              <.link href={~p"/users/log-in"} class="button">
+                Log In To Post
+              </.link>
+            <% end %>
+          </div>
+          <div id="responses" phx-update="stream">
+            <.response
+              :for={{dom_id, response} <- @streams.responses}
+              response={response}
+              id={dom_id}
+            />
+          </div>
+        </div>
         <div class="right">
           <.urgent_incidents incidents={@urgent_incidents} />
         </div>
@@ -79,5 +123,58 @@ defmodule HeadsUpWeb.IncidentLive.Show do
       </.async_result>
     </section>
     """
+  end
+
+  def response(assigns) do
+    ~H"""
+    <div class="response" id={@id}>
+      <span class="timeline"></span>
+      <section>
+        <div class="avatar">
+          <.icon name="hero-user-solid" />
+        </div>
+        <div>
+          <span class="username">
+            {@response.user.username}
+          </span>
+          <span>
+            {@response.status}
+          </span>
+          <blockquote>
+            {@response.note}
+          </blockquote>
+        </div>
+      </section>
+    </div>
+    """
+  end
+
+  def handle_event("validate", %{"response" => response_params}, socket) do
+    changeset = Responses.change_response(%Response{}, response_params)
+
+    socket = assign(socket, :form, to_form(changeset, action: :validate))
+
+    {:noreply, socket}
+  end
+
+  def handle_event("save", %{"response" => response_params}, socket) do
+    %{incident: incident, current_user: user} = socket.assigns
+
+    case Responses.create_response(incident, user, response_params) do
+      {:ok, response} ->
+        changeset = Responses.change_response(%Response{})
+
+        socket =
+          socket
+          |> assign(:form, to_form(changeset))
+          |> stream_insert(:responses, response, at: 0)
+          |> update(:response_count, &(&1 + 1))
+
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, socket}
+    end
   end
 end
